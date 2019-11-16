05513503f0cec5cbd92582fb9cab61111971a56f
nildiert <niljordan23@gmail.com>
2019-11-15 22:08:42 -0500

    [FEATURE] Added basic navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CodeComponent } from './code/code.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CodeComponent
   ],
   imports: [
     BrowserModule,
